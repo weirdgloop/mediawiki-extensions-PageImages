d35c170bb213019086a32c97f905d671a5eb269d
TehKittyCat <tehkittycat@weirdgloop.org>
2022-07-06 01:55:01 -0400

    Use wiki logo as image for main page and as fallback image.

--- includes/PageImages.php
+++ includes/PageImages.php
@@ -250,12 +250,18 @@ class PageImages {
 	 * @param Skin &$skin Skin object used to generate the page. Ignored
 	 */
 	public static function onBeforePageDisplay( OutputPage &$out, Skin &$skin ) {
+		global $wgLogo;
+
+		// WGL - Use wiki logo as image for main page.
+		if ( $out->getContext()->getTitle()->isMainPage() ) {
+			$out->addMeta( 'og:image', wfExpandUrl( $wgLogo, PROTO_CANONICAL ) );
+			return;
+		}
+
 		$imageFile = self::getPageImage( $out->getContext()->getTitle() );
+		// WGL - Use wiki logo as fallback image.
 		if ( !$imageFile ) {
-			$fallback = $out->getConfig()->get( 'PageImagesOpenGraphFallbackImage' );
-			if ( $fallback ) {
-				$out->addMeta( 'og:image', wfExpandUrl( $fallback, PROTO_CANONICAL ) );
-			}
+			$out->addMeta( 'og:image', wfExpandUrl( $wgLogo, PROTO_CANONICAL ) );
 			return;
 		}
 
